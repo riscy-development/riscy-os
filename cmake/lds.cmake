b1192235423309eb19515df75012cc0392e598db
Nikola Maruszewski <nino.maruszewski@gmail.com>
2023-12-07 15:44:38 +0000

    generate linker scripts properly

--- cmake/lds.cmake
+++ cmake/lds.cmake
@@ -1,11 +1,12 @@
 # ----- Linker script support -----
 
-function(add_linker_script target_name script_name)
+macro(add_linker_script target_name script_name)
   add_custom_command(
-    OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target_name}
+    OUTPUT "${target_name}"
     # TODO maybe run the linker scripts through CPP?
-    COMMAND cp ${script_name} ${CMAKE_CURRENT_BINARY_DIR}/${target_name}
-    DEPENDS ${script_name}
-    VERBATIM
+    COMMAND cp "${script_name}" "${target_name}"
+    DEPENDS "${script_name}"
+    COMMENT "Generate linker script ${target_name}"
+    VERBATIM USES_TERMINAL
   )
-endfunction()
+endmacro()
