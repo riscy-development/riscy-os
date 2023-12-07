c2ed36ed89ba4059f6c92727e8efc88de975778f
Nikola Maruszewski <nino.maruszewski@gmail.com>
2023-12-07 19:37:46 +0000

    define `size_t` and `ssize_t`

--- include/kernel/stdint.h
+++ include/kernel/stdint.h
@@ -1,6 +1,8 @@
 #ifndef __KERNEL_STDINT_H__
 #define __KERNEL_STDINT_H__
 
+#include "generated/autoconf.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,13 +14,23 @@ extern "C" {
 typedef char int8_t;
 typedef short int16_t;
 typedef int int32_t;
-typedef long int64_t;
+typedef long long int64_t;
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long uint64_t;
+typedef unsigned long long uint64_t;
 
+#if CONFIG_64_BIT
+typedef uint64_t size_t;
+typedef int64_t ssize_t;
+#else
+typedef uint32_t size_t;
+typedef int32_t ssize_t;
+#endif
+
+/* Checks */
+/* NOLINTBEGIN */
 _Static_assert(sizeof(int8_t) == 1, "int8_t is not 1 byte!");
 _Static_assert(sizeof(int16_t) == 2, "int16_t is not 2 bytes long!");
 _Static_assert(sizeof(int32_t) == 4, "int32_t is not 4 bytes long!");
@@ -28,6 +40,10 @@ _Static_assert(sizeof(uint8_t) == 1, "uint8_t is not 1 byte!");
 _Static_assert(sizeof(uint16_t) == 2, "uint16_t is not 2 bytes long!");
 _Static_assert(sizeof(uint32_t) == 4, "uint32_t is not 4 bytes long!");
 _Static_assert(sizeof(uint64_t) == 8, "uint64_t is not 8 bytes long!");
+
+_Static_assert(sizeof(size_t) == sizeof(sizeof(int)), "size_t not set correctly!");
+_Static_assert(sizeof(ssize_t) == sizeof(sizeof(int)), "size_t not set correctly!");
+/* NOLINTEND */
 
 #ifdef __cplusplus
 }
--- include/kernel/string.h
+++ include/kernel/string.h
@@ -11,8 +11,6 @@ extern "C" {
 #    define NULL 0
 #endif
 
-typedef typeof(sizeof(int)) size_t;
-
 void memcpy(uint8_t* src, uint8_t* dest, size_t len);
 int strlen(char*);
 int strcmp(char* a, char* b);
