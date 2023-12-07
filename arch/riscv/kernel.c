a8bb2b9f47a553b3fcc929e92318a4775a841c05
Nikola Maruszewski <nino.maruszewski@gmail.com>
2023-12-07 21:14:30 +0000

    feat: call global constructors and destructors

--- arch/riscv/kernel.c
+++ arch/riscv/kernel.c
@@ -2,35 +2,98 @@
 #include <kernel/stdint.h>
 #include <kernel/string.h>
 
-unsigned char* uart = (unsigned char*)0x10000000;
+/* Init and Fini arrays */
+typedef void (*func_ptr)(void);
+
+extern func_ptr __preinit_array_start; // NOLINT
+extern func_ptr __preinit_array_end;   // NOLINT
+extern func_ptr __init_array_start;    // NOLINT
+extern func_ptr __init_array_end;      // NOLINT
+extern func_ptr __fini_array_start;    // NOLINT
+extern func_ptr __fini_array_end;      // NOLINT
+
+static void
+process_func_array(func_ptr* start, func_ptr* end) {
+    for (func_ptr* func = start; func != end; func++)
+        (*func)();
+}
+
+static void
+preinit() {
+    /* why do we pass as pointers?
+     *
+     * otherwise gcc dereferences...
+     * but the linker gives us the pointer
+     *
+     * its really horrible, but it was the only way that worked
+     */
+    process_func_array(&__preinit_array_start, &__preinit_array_end);
+}
+
+static void
+init() {
+    process_func_array(&__init_array_start, &__init_array_end);
+}
+
+static void
+fini() {
+    process_func_array(&__fini_array_start, &__fini_array_end);
+}
+
+/* UART Setup */
+unsigned char* UART = (unsigned char*)0x10000000; // NOLINT
 
 void
 putchar(char c) {
-    *uart = c;
-    return;
+    *UART = c;
 }
 
 void
-print(const char* str) {
+puts(const char* str) {
     while (*str != '\0') {
         putchar(*str);
         str++;
     }
-    return;
 }
 
 __attribute__((constructor)) void
 foo(void) {
-    print("foo is running and print is available at this point\n");
+    puts("Hello from a global constructor!\n");
+}
+
+__attribute__((destructor)) void
+bar(void) {
+    puts("Hello from a global destructor!\n");
 }
 
 void
 kmain(uint64_t hartid, struct fdt* fdt) {
+    // Print core ID
+    puts("Running on hart ");
+    putchar((char)hartid + '0');
+    putchar('\n');
+
+    // Check the FDT
     if (verify_fdt(fdt)) {
-        print("Supported FDT Version\n");
+        puts("Using a supported FDT Version\n");
     } else {
-        print("Cannot Read the FDT (Unsupported Version!)\n");
+        puts("Cannot Read the FDT (Unsupported Version!)\n");
     }
-    while (1) {}
-    return;
+
+    // Call global ctors
+    preinit();
+    init();
+
+    for (;;) {
+        putchar(*UART);
+
+        if (*UART == '%')
+            break;
+
+        char old_uart = *UART;
+        while (old_uart == *UART)
+            ;
+    }
+
+    fini();
 }
