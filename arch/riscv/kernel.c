e78faf494fb91e4d9a4e3abc945a750f3b5e9f02
Nikola Maruszewski <nino.maruszewski@gmail.com>
2023-12-07 20:21:23 +0000

    formatting script

--- arch/riscv/kernel.c
+++ arch/riscv/kernel.c
@@ -1,33 +1,36 @@
-#include<kernel/stdint.h>
-#include<kernel/string.h>
-#include<kernel/of/fdt.h>
+#include <kernel/of/fdt.h>
+#include <kernel/stdint.h>
+#include <kernel/string.h>
 
-unsigned char * uart = (unsigned char *)0x10000000; 
-void putchar(char c) {
-	*uart = c;
-	return;
+unsigned char* uart = (unsigned char*)0x10000000;
+
+void
+putchar(char c) {
+    *uart = c;
+    return;
 }
 
-void print(const char * str) {
-	while(*str != '\0') {
-		putchar(*str);
-		str++;
-	}
-	return;
+void
+print(const char* str) {
+    while (*str != '\0') {
+        putchar(*str);
+        str++;
+    }
+    return;
 }
 
-__attribute__ ((constructor)) void foo(void)
-{
-	print("foo is running and print is available at this point\n");
+__attribute__((constructor)) void
+foo(void) {
+    print("foo is running and print is available at this point\n");
 }
 
-void kmain(uint64_t hartid, struct fdt *fdt) {
-	if(verify_fdt(fdt)) {
-		print("Supported FDT Version\n");
-	} else {
-		print("Cannot Read the FDT (Unsupported Version!)\n");
-	}
-	while(1) {
-	}
-	return;
+void
+kmain(uint64_t hartid, struct fdt* fdt) {
+    if (verify_fdt(fdt)) {
+        print("Supported FDT Version\n");
+    } else {
+        print("Cannot Read the FDT (Unsupported Version!)\n");
+    }
+    while (1) {}
+    return;
 }
---
+++ scripts/format.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+shopt -s extglob
+
+PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$( realpath "$PWD/.." )"
+
+clang-format -i \
+    "$ROOT"/arch/**/*.{c,cpp,h,hpp} \
+    "$ROOT"/include/**/*.{c,cpp,h,hpp} \
+    "$ROOT"/kernel/**/*.{c,cpp,h,hpp} 
