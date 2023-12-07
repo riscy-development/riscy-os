6178b480d6f0fff53814f86f4b95f3613d0059b0
Nikola Maruszewski <nino.maruszewski@gmail.com>
2023-12-07 15:44:13 +0000

    use proper debugging flags

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -16,6 +16,6 @@ set(CMAKE_CXX_FLAGS "${DEFAULT_CXX_FLAGS} ${CMAKE_CXX_FLAGS}")
 
 # Add debug flags if needed
 if (CONFIG_DEBUG_SYMBOLS)
-  set(CMAKE_C_FLAGS "-g4 ${CMAKE_C_FLAGS}")
-  set(CMAKE_CXX_FLAGS "-g4 ${CMAKE_CXX_FLAGS}")
+  set(CMAKE_C_FLAGS "-g3 ${CMAKE_C_FLAGS}")
+  set(CMAKE_CXX_FLAGS "-g3 ${CMAKE_CXX_FLAGS}")
 endif()
