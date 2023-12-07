1ac4ebf5f016ffbcfece5171cf20b455a213a75f
Nikola Maruszewski <nino.maruszewski@gmail.com>
2023-12-07 19:22:37 +0000

    move `-nostdinc` compiler flag to globally apply

--- toolchain/flags.cmake
+++ toolchain/flags.cmake
@@ -1,11 +1,11 @@
 # Set some basic compiler flags
 set(
   CMAKE_C_FLAGS
-  "-ffreestanding -nostdlib -fdiagnostics-color=always -fno-common ${CMAKE_C_FLAGS}"
+  "-ffreestanding -nostdlib -nostdinc -fdiagnostics-color=always -fno-common ${CMAKE_C_FLAGS}"
 )
 set(
   CMAKE_CXX_FLAGS
-  "-ffreestanding -nostdlib -nostdinc++ -fdiagnostics-color=always -fno-common -fno-rtti -fno-exceptions ${CMAKE_CXX_FLAGS}"
+  "-ffreestanding -nostdlib -nostdinc -nostdinc++ -fdiagnostics-color=always -fno-common -fno-rtti -fno-exceptions ${CMAKE_CXX_FLAGS}"
 )
 
 add_link_options(LINKER:--gc-sections)
