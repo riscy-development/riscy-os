36d2c09ba98bd1d70cc9ac5a31851a3cd4dba520
Nikola Maruszewski <nino.maruszewski@gmail.com>
2023-12-08 19:28:07 +0000

    add other exit functions to stdlib header

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -5,6 +5,8 @@
 extern "C" {
 #endif
 
+#warning The stdlib header is not complete.
+
 /**
  * Causes abnormal program termination.
  *
@@ -15,6 +17,18 @@ extern "C" {
  * This function does not return.
  */
 [[noreturn]] void abort();
+
+#if 0 /* NOLINT: TODO */
+[[noreturn]] void exit(int exit_code);
+[[noreturn]] void quick_exit(int exit_code);
+[[noreturn]] void _Exit(int exit_code);
+
+int atexit(void (*func)(void));
+int at_quick_exit(void (*func)(void));
+
+#  define EXIT_SUCCESS /*implementation defined*/
+#  define EXIT_FAILURE /*implementation defined*/
+#endif
 
 #ifdef __cplusplus
 } /* extern "C" */
