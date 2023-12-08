7a6cfcbdaab47f62d9fc8653d84f7567d478a8df
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2023-12-08 22:01:15 +0000

    Added kernel/error.h
    Provides some generic KERR_- codes which can be used within different internal kernel API's

---
+++ include/kernel/error.h
@@ -0,0 +1,35 @@
+#ifndef __KERNEL_ERROR_H__
+#define __KERNEL_ERROR_H__
+
+/*
+ * Defines Common Error Codes which can be used by many different internal kernel API's
+ *
+ * Each API needs to define exactly what each error code means but these enum(s) should
+ * fit the vast majority of use cases
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum kerror {
+  KERR_NONE = 0, // No errors occurred
+  KERR_SUCCESS = KERR_NONE,
+
+  KERR_EXIST, // Trying to create something which already exists
+  KERR_NO_EXIST, // Trying to use something which doesn't exist
+
+  KERR_FULL, // Trying to insert/push into a full data structure
+  KERR_EMPTY, // Trying to read/pop from an empty data structure
+
+  KERR_BUSY, // Less so an error, but an opportunity to do other work and try again later
+
+  KERR_UNKNOWN, // Unknown error (catch all DO NOT USE WITHOUT GOOD REASON)
+};
+typedef enum kerror kerror_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
