e9722f4ca46ebf0a69c9c8d6e6331c72eef79523
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2023-12-09 17:47:57 +0000

    Added va_copy

--- libc/include/stdarg.h
+++ libc/include/stdarg.h
@@ -8,9 +8,19 @@ extern "C" {
 #ifdef __GNUC__
 
 typedef __builtin_va_list va_list;
-#define va_start __builtin_va_start
-#define va_end __builtin_va_end
-#define va_arg __builtin_va_arg
+
+#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
+#define va_start(v, ...)	__builtin_va_start(v, 0)
+#else
+#define va_start(v,l)	__builtin_va_start(v,l)
+#endif
+#define va_end(v)	__builtin_va_end(v)
+#define va_arg(v,l)	__builtin_va_arg(v,l)
+#if !defined(__STRICT_ANSI__) || __STDC_VERSION__ + 0 >= 199900L \
+    || __cplusplus + 0 >= 201103L
+#define va_copy(d,s)	__builtin_va_copy(d,s)
+#endif
+#define __va_copy(d,s)	__builtin_va_copy(d,s)
 
 #else
 #    error Only compatible with GCC right now.
