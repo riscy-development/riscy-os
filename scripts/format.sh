a63f951b7c8567a4f9db2a23e2f7ee05ff31da53
Nikola Maruszewski <nino.maruszewski@gmail.com>
2023-12-09 19:01:47 +0000

    ci: also check drivers and libc

--- scripts/format.sh
+++ scripts/format.sh
@@ -10,4 +10,6 @@ ROOT="$( realpath "$PWD/.." )"
 clang-format -i \
     "$ROOT"/arch/**/*.{c,cpp,h,hpp} \
     "$ROOT"/include/**/*.{c,cpp,h,hpp} \
-    "$ROOT"/kernel/**/*.{c,cpp,h,hpp} 
+    "$ROOT"/drivers/**/*.{c,cpp,h,hpp} \
+    "$ROOT"/kernel/**/*.{c,cpp,h,hpp} \
+    "$ROOT"/libc/**/*.{c,cpp,h,hpp}
